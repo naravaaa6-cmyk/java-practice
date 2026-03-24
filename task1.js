2925f66808d9ad21f1e89c7fb111496eb691cb3a
naravaaa6-cmyk <naravaaa6@gmail.com>
2026-03-24 22:46:04 +0400

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,9 @@
+const companyName = "Google";
+let visitCount = 1;
+let userIsActive = true;
+let something;
+
+console.log(companyName, typeof companyName);
+console.log(visitCount, typeof visitCount);
+console.log(userIsActive, typeof userIsActive);
+console.log(something, typeof something);
